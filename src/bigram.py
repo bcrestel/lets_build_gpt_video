8738c5414ca1a56231f0ffdb0cbb47594fbef654
Ben Crestel <ben.crestel@zoho.com>
2023-02-20 07:13:01 -0500

    continue notebook and fix bug within loss function

--- src/bigram.py
+++ src/bigram.py
@@ -1,5 +1,7 @@
 # n_gram.py
 
+from typing import Optional
+
 import torch
 import torch.nn as nn
 from torch.nn import functional
@@ -8,10 +10,11 @@ from src.text_processor import TextProcessor
 
 
 class BiGram(nn.Module):
-    def __init__(self, vocab_size: int):
+    def __init__(self, vocab_size: int, device: Optional[torch.device] = None):
         super().__init__()
         self.vocab_size = vocab_size
         self.embedding = nn.Embedding(self.vocab_size, self.vocab_size)
+        self.device = ("cuda" if torch.cuda.is_available() else 'cpu') if device is None else device
 
     def forward(self, x: torch.Tensor):
         """x.shape = (B, T)"""
@@ -23,6 +26,9 @@ class BiGram(nn.Module):
         logits.shape = (B, T, vocab_size)
         y.shape = (B, T)
         """
-        logits = logits.view((-1, self.vocab_size))
-        y = y.view((-1, self.vocab_size))
+        logits = logits.view(-1, self.vocab_size)
+        y = y.view(-1)
         return functional.cross_entropy(logits, y)
+
+    # TODO: Continuere here
+    #def train(self, epochs: int):
