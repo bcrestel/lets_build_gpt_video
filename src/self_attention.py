1708db7a6987cc8463cd860ff2a5b397ce765b01
Ben Crestel <ben.crestel@zoho.com>
2023-03-08 06:59:44 -0500

    add dropout

--- src/self_attention.py
+++ src/self_attention.py
@@ -3,13 +3,22 @@ import torch.nn as nn
 from torch.nn import functional 
 
 class ScaledDotProductSelfAttentionHead(nn.Module):
-    def __init__(self, dim_token_embedding: int, head_size: int, max_block_size: int):
+    def __init__(self, dim_token_embedding: int, head_size: int, max_block_size: int, dropout_rate: float = 0.0):
+        """_summary_
+
+        Args:
+            dim_token_embedding (int): 
+            head_size (int): dimensions of K, Q, V
+            max_block_size (int): 
+            dropout_rate (float, optional): Apply dropout to the attention weights. Defaults to 0.0 (no dropout). Note dropout rescale entries at training time
+        """
         super().__init__()
         self.key_layer = nn.Linear(dim_token_embedding, head_size, bias=False)
         self.query_layer = nn.Linear(dim_token_embedding, head_size, bias=False)
         self.value_layer = nn.Linear(dim_token_embedding, head_size, bias=False)
         self.register_buffer('mask', torch.tril(torch.ones(max_block_size, max_block_size)))
         self.scaling_factor = head_size ** (-0.5)
+        self.dropout = nn.Dropout(dropout_rate)
     
     def forward(self, x: torch.Tensor) -> torch.Tensor:
         """_summary_
@@ -24,9 +33,8 @@ class ScaledDotProductSelfAttentionHead(nn.Module):
         mask = self.mask[:block_size, :block_size]
         weights.masked_fill_(mask==0, float("-inf"))
         weights = functional.softmax(weights, dim=-1)
+        weights = self.dropout(weights)
 
         V = self.value_layer(x)
         output = weights @ V # shape (B, block, block) @ (B, block, emb) -> (B, block, emb)
         return output
-
-        
