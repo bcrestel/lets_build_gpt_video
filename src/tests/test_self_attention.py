177d371cb9a442a1b0d2197d120b97eb35405466
Ben Crestel <ben.crestel@zoho.com>
2023-03-17 23:13:02 -0400

    add more tests

--- src/tests/test_self_attention.py
+++ src/tests/test_self_attention.py
@@ -1,20 +1,40 @@
 import pytest
 import torch
 
-from src.self_attention import ScaledDotProductSelfAttentionHead
+from src.self_attention import ScaledDotProductSelfAttentionHead, MultiHeadAttention
 
 @pytest.mark.parametrize("block_size", [8, 12])
 @pytest.mark.parametrize("dropout_rate", [0.0, 0.2])
-def test_selfattentionhead(dropout_rate, block_size):
+@pytest.mark.parametrize("dim_tok_emb", [256, 512])
+@pytest.mark.parametrize("head_size", [32, 64])
+def test_selfattentionhead(dropout_rate, block_size, dim_tok_emb, head_size):
     sa = ScaledDotProductSelfAttentionHead(
-        dim_token_embedding=512,
-        head_size=64,
+        dim_token_embedding=dim_tok_emb,
+        head_size=head_size,
         max_block_size=12,
         dropout_rate=dropout_rate,
     )
-    x = torch.randn(32, block_size, 512)
+    batch_size = 16
+    x = torch.randn(batch_size, block_size, dim_tok_emb)
     out = sa(x)
-    assert out.shape == (32, block_size, 64)
+    assert out.shape == (batch_size, block_size, head_size)
 
 
-def test_multiheadattention():
+@pytest.mark.parametrize("block_size", [8, 12])
+@pytest.mark.parametrize("dropout_rate", [0.0, 0.2])
+@pytest.mark.parametrize("dim_tok_emb", [256, 512])
+@pytest.mark.parametrize("nb_heads", [6, 12])
+@pytest.mark.parametrize("head_size", [64, 128])
+def test_multiheadattention(block_size, dropout_rate, dim_tok_emb, nb_heads, head_size):
+    ma = MultiHeadAttention(
+        nb_heads=nb_heads, 
+        dim_token_embedding=dim_tok_emb,
+        head_size=head_size,
+        max_block_size=12,
+        dropout_rate=dropout_rate
+    )
+    batch_size = 16
+    x = torch.randn(batch_size, block_size, dim_tok_emb)
+    out = ma(x)
+    assert out.shape == (batch_size, block_size, nb_heads*head_size)
+
